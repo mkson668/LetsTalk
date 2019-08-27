be204b092a0b0ec3d8611798aa1bd799286ff8bb
aaron <z0i0b@ugrad.cs.ubc.ca>
2019-08-26 21:59:59 -0700

    Initial Commit

---
+++ LetsTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LetsTalk
+//
+//  Created by Aaron on 2019-08-26.
+//  Copyright © 2019 Aaron Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
